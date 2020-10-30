619e3ee3c9430069fb00b633aa13cb79058da392
smaug <smaug.jiao@topnetwork.org>
2020-10-30 11:02:28 +0800

    update for multi argv

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
- g++ main.cc  setproctitle.cc  -o main -std=c++11
+ g++ main.cc  setproctitle.cc  -o main -std=c++11 -g
--- main.cc
+++ main.cc
@@ -3,19 +3,23 @@
 #include <cstring>
 #include <unistd.h>
 
+#include <string>
+#include <iostream>
+
 #include "setproctitle.h"
 
 char **smaug_os_argv;
 
 void dump_argv_env(const char* process_id, int argc, char *argv[], char *envp[]) {
     int i;
-    for (i = 0; i < argc; ++i) {
+    for (i = 0; i < argc && argv[i]; ++i) {
         printf("%s mem:%p len:%d argv[%d]: %s\n", process_id, argv[i], strlen(argv[i]), i, argv[i]);
     }
     printf("\n");
     for (i = 0; envp[i] != NULL; ++i) {
         printf("%s mem:%p len:%d envp[%d]: %s\n", process_id, envp[i], strlen(envp[i]), i, envp[i]);
     }
+    printf("\n");
 }
 
 int worker_work() {
@@ -46,22 +50,56 @@ int main(int argc, char* argv[], char *envp[])
             }
          case 0:
             {
+                sleep(1);
                 printf("\n\n#######this is child process###########\n");
-                const char *new_title = "setproctitle: worker";
-                if (smaug_init_setproctitle() == SMAUG_PROCTITLE_OK) {
-                    smaug_setproctitle(new_title);
+                dump_argv_env("old_worker", argc, argv, envp);
+
+                const char *new_process_name = "setproctitle: worker";
+                std::string new_title;
+                for (int i = 0; i < argc && argv[i]; ++i) {
+                    if (i == 0) {
+                        new_title += new_process_name;
+                        new_title += " ";
+                    } else {
+                        new_title += argv[i];
+                        new_title += " ";
+                    }
                 }
-                dump_argv_env("worker", argc, argv, envp);
+                if (new_title.back() == ' ') {
+                    new_title.pop_back();
+                }
+
+                if (smaug_init_setproctitle() == SMAUG_PROCTITLE_OK) {
+                    smaug_setproctitle(new_title.c_str());
+                }
+
+                dump_argv_env("new_worker", argc, argv, envp);
                 return worker_work();
             }
          default:
             {
                 printf("\n\n#######this is father process###########\n");
-                const char *new_title = "setproctitle: master";
-                if (smaug_init_setproctitle() == SMAUG_PROCTITLE_OK) {
-                    smaug_setproctitle(new_title);
+                dump_argv_env("old_master", argc, argv, envp);
+
+                const char *new_process_name = "setproctitle: master";
+                std::string new_title;
+                for (int i = 0; i < argc && argv[i]; ++i) {
+                    if (i == 0) {
+                        new_title += new_process_name;
+                        new_title += " ";
+                    } else {
+                        new_title += argv[i];
+                        new_title += " ";
+                    }
                 }
-                dump_argv_env("master", argc, argv, envp);
+                if (new_title.back() == ' ') {
+                    new_title.pop_back();
+                }
+
+                if (smaug_init_setproctitle() == SMAUG_PROCTITLE_OK) {
+                    smaug_setproctitle(new_title.c_str());
+                }
+                dump_argv_env("new_master", argc, argv, envp);
                 return master_work();
             }
     }
